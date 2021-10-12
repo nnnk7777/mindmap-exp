7f18539f3db24e4d345085dfc9c060ba5b6102a3
nonaka <electro0701@gmail.com>
2021-10-12 13:10:25 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VNetworkGraph from "v-network-graph";
+import "v-network-graph/lib/style.css";
 
-createApp(App).use(store).use(router).mount('#app')
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD4APe-XvwV5t1nRb8DrSD77l9SHeCdC4",
+  authDomain: "mindmap-test-5701e.firebaseapp.com",
+  projectId: "mindmap-test-5701e",
+  storageBucket: "mindmap-test-5701e.appspot.com",
+  messagingSenderId: "809087588158",
+  appId: "1:809087588158:web:4decfc8bf96a0d168b7e6f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(VNetworkGraph)
+  .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,124 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-network-graph
+      :nodes="nodes"
+      :edges="edges"
+      :layouts="layouts"
+      :configs="configs"
+      @click="clickedView"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { GridLayout } from "v-network-graph";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      data: [
+        {
+          ID: 0,
+          Label: "root",
+          Parent: null,
+          Shape: "rect",
+          Color: "#cc4444",
+          Draggable: false,
+          User: null,
+        },
+        {
+          ID: 1,
+          Label: "node1",
+          Parent: 0,
+          User: "nonaka",
+        },
+        {
+          ID: 2,
+          Label: "node2",
+          Parent: 0,
+          User: "inu",
+        },
+        {
+          ID: 3,
+          Label: "node3",
+          Parent: 0,
+          User: "nonaka",
+        },
+        {
+          ID: 4,
+          Label: "node4",
+          Parent: 2,
+          User: "inu",
+        },
+        {
+          ID: 5,
+          Label: "node5",
+          Parent: 3,
+          User: "nonaka",
+        },
+        {
+          ID: 6,
+          Label: "node6",
+          Parent: 3,
+          User: "nonaka",
+        },
+      ],
+    };
+  },
+  computed: {
+    nodes() {
+      let _nodes = {};
+      this.data.forEach((d) => {
+        let tmp = {};
+        tmp.name = d.Label;
+        if (d.Shape) tmp.type = d.Shape;
+        if (d.Color) tmp.color = d.Color;
+        if (d.Draggable != null) tmp.draggable = d.Draggable;
+        _nodes[`node${d.ID}`] = tmp;
+      });
+      return _nodes;
+    },
+    edges() {
+      let _edges = {};
+      this.data.forEach((d) => {
+        if (d.Parent != null) {
+          let tmp = {};
+          tmp.source = `node${d.Parent}`;
+          tmp.target = `node${d.ID}`;
+          _edges[`edge${d.ID}`] = tmp;
+        }
+      });
+      return _edges;
+    },
+    layouts() {
+      // let _layouts = {};
+      // 下のようなレイアウトをthis.dataから生成する
+      return {
+        nodes: {
+          node0: { x: 0, y: 0 },
+          node1: { x: 100, y: 50 },
+          node2: { x: -50, y: 50 },
+          node3: { x: 100, y: -50 },
+          node4: { x: -100, y: 50 },
+          node5: { x: 180, y: -100 },
+          node6: { x: 180, y: 0 },
+        },
+      };
+    },
+    configs() {
+      return {
+        node: {
+          normal: {
+            type: (node) => node.type || "circle",
+            color: (node) => node.color || "#4466cc",
+          },
+        },
+        view: {
+          layoutHandler: new GridLayout({ grid: 20 }),
+        },
+      };
+    },
+  },
+};
 </script>
