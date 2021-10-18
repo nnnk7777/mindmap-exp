aa3acf7abcffc8abbe99fb322f9a05ac128fa6a0
nonaka <electro0701@gmail.com>
2021-10-18 23:40:16 +0900

    storeに必要なデータが保存されるように

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,31 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    mapName: null,
+    userName: null,
+    userRole: null,
   },
   mutations: {
+    SET_MAP_NAME(state, val) {
+      state.mapName = val;
+    },
+    SET_USER_NAME(state, val) {
+      state.userName = val;
+    },
+    SET_USER_ROLE(state, val) {
+      state.userRole = val;
+    },
   },
   actions: {
+    setMapName({ commit }, name) {
+      commit("SET_MAP_NAME", name);
+    },
+    setUserName({ commit }, name) {
+      commit("SET_USER_NAME", name);
+    },
+    setUserRole({ commit }, role) {
+      commit("SET_USER_ROLE", role);
+    },
   },
-  modules: {
-  }
-})
+});
